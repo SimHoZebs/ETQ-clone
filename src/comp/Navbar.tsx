7700cb89ed64d8f441cbf09bd84b6707d243763d
SimHoZebs <simhozebs@gmail.com>
2022-03-03 23:43:31 +0900

    mobile menu

--- src/comp/Navbar.tsx
+++ src/comp/Navbar.tsx
@@ -1,9 +1,12 @@
 import { Icon } from "@iconify/react";
 import { useEffect, useState } from "preact/hooks";
 
+import data from "../data";
+
 const Navbar = () => {
   const [atTop, setAtTop] = useState(true);
   const [scrolling, setScrolling] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(true);
 
   useEffect(() => {
     window.addEventListener("scroll", onScroll);
@@ -16,61 +19,87 @@ const Navbar = () => {
 
   function onScroll() {
     setScrolling(true);
-
-    if (document.documentElement.scrollTop !== 0) {
-      setAtTop(false);
-    } else {
-      setAtTop(true);
-    }
+    setAtTop(document.documentElement.scrollTop !== 0 ? false : true);
 
     if (timer !== null) {
       clearTimeout(timer);
     }
 
     timer = setTimeout(() => {
-      console.log("yo");
       setScrolling(false);
-    }, 300);
+    }, 1000);
+  }
+  function openMenu() {
+    setMenuOpen(!menuOpen);
   }
 
   return (
     <nav
-      class={`fixed left-0 top-0 m-1 flex w-[calc(100%-0.5rem)] items-center justify-between py-1 px-4 lg:(px-8 py-4 m-2) ${
+      class={`fixed left-0 top-0 m-1 w-[calc(100%-0.5rem)] py-1 lg:(px-8 py-4 m-2) ${
         atTop ? "" : ""
-      } ${
-        (scrolling && !atTop) || (!scrolling && atTop)
-          ? ""
-          : "duration-1000 opacity-0"
-      }`}
+      } ${scrolling || atTop || menuOpen ? "" : "duration-1000 opacity-0"}`}
     >
-      <button class="flex h-8 w-8 items-center py-1 lg:hidden">
-        <Icon icon="mdi:menu" width="36" height="36" />
-      </button>
-
-      <div class="flex gap-x-4 items-center">
-        <h1 class="text-3xl font-black">Company</h1>
-        <div class="<lg:hidden flex gap-x-4 self-center">
-          <button>Shop All</button>
-          <button>Permanent Collection</button>
-          <button>Shoecare</button>
-        </div>
-      </div>
-
-      <div class="flex gap-x-4">
-        <div class="flex gap-x-4 <lg:hidden">
-          <button>Search</button>
-          <button>Help</button>
-          <button>My Account</button>
-        </div>
-        <button class="h-6 w-6 rounded-full bg-black text-xs text-white">
-          0
+      <div class="flex items-center justify-between w-full px-4 py-2">
+        <button
+          class="flex h-8 w-8 items-center py-1 lg:hidden"
+          onClick={openMenu}
+        >
+          <Icon icon="mdi:menu" width="36" height="36" />
         </button>
+        <div class="flex gap-x-4 items-center">
+          <h1 class="text-3xl font-black pr-8">Logo</h1>
+
+          <div class="<lg:hidden flex gap-x-4 self-center">
+            <button>Shop All</button>
+            <button>Permanent Collection</button>
+            <button>Shoecare</button>
+          </div>
+        </div>
+        <div class="flex gap-x-4">
+          <div class="flex gap-x-4 <lg:hidden">
+            <button>Search</button>
+            <button>Help</button>
+            <button>My Account</button>
+          </div>
+
+          <button class="h-6 w-6 rounded-full bg-black text-xs text-white">
+            0
+          </button>
+        </div>
+
+        <div
+          class={`absolute top-0 left-0 -z-10 h-full w-full duration-150 ${
+            atTop && !menuOpen ? "" : "bg-white opacity-95"
+          }`}
+        ></div>
       </div>
+
       <div
-        class={`absolute top-0 left-0 -z-10 h-full w-full duration-150 ${
-          atTop ? "" : "bg-white opacity-95"
+        class={`lg:hidden duration-200 bg-white flex flex-col overflow-hidden items-start ${
+          menuOpen ? "max-h-screen" : "max-h-0 gap-y-0 p-0"
         }`}
-      ></div>
+      >
+        {data.menu.map((item, i) => (
+          <div key={i} class="w-full">
+            <div class="flex justify-between items-center px-4 py-6">
+              <button class="border-b duration-300 hover:(border-black) border-white">
+                {item}
+              </button>
+
+              {item !== "Search" ? (
+                item === "Shoecare" ? (
+                  ""
+                ) : (
+                  <Icon icon="akar-icons:chevron-right" />
+                )
+              ) : (
+                <Icon icon="mdi:magnify" />
+              )}
+            </div>
+            <hr class="border-t w-full border-t-gray-200" />
+          </div>
+        ))}
+      </div>
     </nav>
   );
 };
--- src/data.ts
+++ src/data.ts
@@ -9,6 +9,7 @@ const data = {
   favs: "https://assets.adidas.com/images/w_600,f_auto,q_auto/ce8a6f3aa6294de988d7abce00c4e459_9366/Breaknet_Shoes_White_FX8707_01_standard.jpg"
   ,
   store: { imgURL: " https://cdn.shopify.com/s/files/1/0505/9044/9849/files/Laundry_Store_Homepage-image_1920x.jpg?v=1631259918 ", name: "ETQ Laundry Store", desc: "Let us clean your shoes" }
+  , menu: ["Shop All", "Permanent Collection", "Shoecare", "Help", "Search"]
 };
 
 export default data;
