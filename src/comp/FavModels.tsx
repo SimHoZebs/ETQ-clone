beda733739d9156dc7acbb4c5712bb508e390bf1
SimHoZebs <simhozebs@gmail.com>
2022-03-04 18:48:11 +0900

    buttons disabled when needed

--- src/comp/FavModels.tsx
+++ src/comp/FavModels.tsx
@@ -26,31 +26,48 @@ const FavSection = () => {
   return (
     <section class=" flex flex-col px-4 lg:px-8 relative">
       <button
-        class="absolute left-4 top-1/2 md:hidden rounded-1 border border-gray-100 bg-gray-100 opacity-70 p-2"
+        class="disabled:hidden absolute left-4 top-1/2 md:hidden rounded-1 border border-gray-100 bg-gray-100 opacity-70 p-2"
         onClick={() => scrollTo(-1)}
+        disabled={currIndex === 0}
       >
         <Left />
       </button>
 
       <button
-        class="absolute right-4 top-1/2 rounded-1 md:hidden border border-gray-100 bg-gray-100 opacity-70 p-2"
+        class="absolute right-4 top-1/2 rounded-1 md:hidden border border-gray-100 bg-gray-100 opacity-70 p-2 disabled:hidden"
         onClick={() => scrollTo(1)}
+        disabled={
+          favSectionRef.current
+            ? currIndex === favSectionRef.current.children.length - 1
+              ? true
+              : false
+            : false
+        }
       >
         <Right />
       </button>
+
       <div class="flex flex-row items-center justify-between">
         <h2 class="text-2xl">Our favorite models</h2>
         <div class="flex gap-x-2">
           <button
-            class="rounded-1 border <md:hidden border-gray-300 p-2"
+            class="rounded-1 border <md:hidden border-gray-300 p-2 disabled:opacity-40 duration-200"
             onClick={() => scrollTo(-2)}
+            disabled={currIndex === 0}
           >
             <Left />
           </button>
 
           <button
-            class="rounded-1 <md:hidden border border-gray-300 p-2"
+            class="rounded-1 <md:hidden border border-gray-300 p-2 duration-200 disabled:opacity-40"
             onClick={() => scrollTo(2)}
+            disabled={
+              favSectionRef.current
+                ? currIndex === favSectionRef.current.children.length - 2
+                  ? true
+                  : false
+                : false
+            }
           >
             <Right />
           </button>
